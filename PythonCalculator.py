af3a92c12ee82843bc121de13f42bb77431781e3
leandroniels4-glitch <leandro.niels4@gmail.com>
2025-11-27 00:29:49 +0000

    Now any input can be processed, even invalid ones

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -10,6 +10,18 @@ while calc_on:
 
 #2 Need to create a menu that the user can access showcasing all the functionalities of the calculator
     calculate: str = input('Please enter a calculation in the form \"x\" \"operator\" \"y\": ').lower()
+
+#6 Adding incorrect input help
+    valid_input = False
+    while valid_input == False:
+        calc_parts = calculate.split(' ')
+        if calculate in ['operations', 'o', 'end', 'e',]:
+            valid_input = True
+        elif len(calc_parts) == 3 and type(float(calc_parts[0])) == float and type(str(calc_parts[1])) == str and type(float(calc_parts[2])) == float:
+            valid_input = True
+        else:
+            valid_input = False
+            calculate: str = input('Sorry, please ensure you enter a calculation in the form \"x\" \"operator\" \"y\": ').lower()
     if calculate in ['end', 'e']:
         calc_on = False
     elif calculate in ['operations', 'o']:
@@ -22,8 +34,10 @@ while calc_on:
         num1 = float(calc_parts[0])
         operator = str(calc_parts[1])
         num2 = float(calc_parts[2])
-        #print(f'{num1}\n{operator}\n{num2}') -- simple check
 #4 Creating addition functionality
         if operator == '+':
             print(f'{num1} + {num2} = {num1 + num2}')
+#5 Creating subtraction functionality
+        if operator == '-':
+            print(f'{num1} - {num2} = {num1 - num2}')
 print('Thank you for using the calculator, goodbye!')

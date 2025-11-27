2f926fcb819cf731bc0e81c33a2ee33fc930aebf
leandroniels4-glitch <leandro.niels4@gmail.com>
2025-11-27 11:59:13 +0000

    Finished functionality of other operators, removed need for entering space in inputter

--- PythonCalculator.py
+++ PythonCalculator.py
@@ -1,20 +1,20 @@
 #1 The goal is to create a simple calculator
 import math
+import re
 print('Welcome to the python calculator!')
 print('If you wish to see what operations are possible please enter: \"operations\" or \"o\"')
-
 #3 Make the calculator allows an input until its terminated
 print('If you wish to close the calculator please enter \"end\" or \"e\"')
 calc_on = True
 while calc_on:
-
 #2 Need to create a menu that the user can access showcasing all the functionalities of the calculator
-    calculate: str = input('Please enter a calculation in the form \"x\" \"operator\" \"y\": ').lower()
-
+    calculate: str = input('Please enter a calculation in the form \"x\"\"operator\"\"y\": ').lower()
 #6 Adding incorrect input help
     valid_input = False
     while valid_input == False:
-        calc_parts = calculate.split(' ')
+#9 Making it so the user doesn't have to enter a space
+        calc_parts = re.findall(r'\d+\.\d+|\d+|[+\-^*(rt)]+', calculate.replace(' ', '')    )
+        print(calc_parts)
         if calculate in ['operations', 'o', 'end', 'e',]:
             valid_input = True
         elif len(calc_parts) == 3 and type(float(calc_parts[0])) == float and type(str(calc_parts[1])) == str and type(float(calc_parts[2])) == float:
@@ -26,11 +26,9 @@ while calc_on:
         calc_on = False
     elif calculate in ['operations', 'o']:
         print('This is a comprehensive list of all the operations you can perform. \nThis list is in the following format: \"operator\", \"symbol\"')
-        print('Addition, +\nSubtraction, -\nMultiplication, *\nDivision, /\nExponential, ^\nSquare root, sqrt()')
-
+        print('Addition, +\nSubtraction, -\nMultiplication, *\nDivision, /\nExponential, ^\nRoot, rt')
 #3 Splitting the inputs so the calculator can interpret each character individually
     else:
-        calc_parts = calculate.split(' ')
         num1 = float(calc_parts[0])
         operator = str(calc_parts[1])
         num2 = float(calc_parts[2])
@@ -40,4 +38,14 @@ while calc_on:
 #5 Creating subtraction functionality
         if operator == '-':
             print(f'{num1} - {num2} = {num1 - num2}')
+#7 Creating other operatior functionality
+        if operator == '*':
+            print(f'{num1}*{num2} = {num1*num2}')
+        if operator == '/':
+            print(f'{num1}/{num2} = {num1/num2}')
+#8 Creating exp and root functionality
+        if operator == '^':
+            print(f'{num1}^{num2} = {math.pow(num1, num2)}')
+        if operator == 'rt':
+            print(f'{num1}root{num2} = {math.pow(num1, 1/num2)}')
 print('Thank you for using the calculator, goodbye!')
